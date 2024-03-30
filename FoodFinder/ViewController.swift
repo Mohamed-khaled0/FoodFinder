abcb95a5d529890f2fe1c8619b8cc251bf5aeb97
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-03-30 03:54:05 +0200

    Initial commit

---
+++ FoodFinder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodFinder
+//
+//  Created by MoKhaled on 30/03/2024.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
