a106172105d2ba533766fdc7fd2e423bbe1b7b48
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-03-31 03:06:15 +0200

    detect image function

--- FoodFinder/ViewController.swift
+++ FoodFinder/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import CoreML
 import Vision
 
+
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +20,13 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     @IBAction func cameraTapped(_ sender: Any) {
         
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("can't load ML model")
+        }
     }
 }
