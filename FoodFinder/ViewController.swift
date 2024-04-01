abe56c30e89c86b91c94839bbd14315a7a7fae2f
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-04-01 18:12:59 +0200

    Add Icon

--- FoodFinder/ViewController.swift
+++ FoodFinder/ViewController.swift
@@ -10,23 +10,41 @@ import CoreML
 import Vision
 
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let imagePicker = UIImagePickerController()
+
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
     }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard  let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("Could not convert to CIImage")
+                
+            }
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+  
     
     @IBAction func cameraTapped(_ sender: Any) {
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    func detect(image: CIImage) {
-        
-        // Load the ML model through its generated class
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
-            fatalError("can't load ML model")
-        }
+
     }
-}
+
